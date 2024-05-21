12cd5cf8fe682f52497ebaad5e1b80ab5d0c95e6
Kim JaeMin <2098kjm@naver.com>
2024-05-21 11:49:22 +0900

    test

--- src/test/java/com/example/devopsboard/DevopsBoardApplicationTests.java
+++ src/test/java/com/example/devopsboard/DevopsBoardApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.devopsboard;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevopsBoardApplicationTests {
+	@Value("${spring.datasource.username}") String name;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void dbUserNameIsSa(){
+		assertEquals(name, "sa");
+	}
 }
