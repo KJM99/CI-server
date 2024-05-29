5a8147ae8e184f2057d3933afeccc5f96f68f3e0
Kim JaeMin <2098kjm@naver.com>
2024-05-29 11:44:14 +0900

    test

--- src/main/java/com/example/devopsboard/controller/BoardController.java
+++ src/main/java/com/example/devopsboard/controller/BoardController.java
@@ -17,13 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/boards")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173"
+@CrossOrigin(origins = "http://35.193.99.200/"
     ,methods = {
     RequestMethod.GET,
     RequestMethod.POST,
     RequestMethod.DELETE,
     RequestMethod.PUT,
-    RequestMethod.OPTIONS})
+    RequestMethod.OPTIONS}, allowedHeaders = "*")
 public class BoardController {
 
     private final BoardService boardService;
